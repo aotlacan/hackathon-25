015fe903c41b823bf9b5cf36f0c8656c945ca9a2
root <dyang16688@gmail.com>
2025-09-27 19:01:59 -0400

    Google Maps integration

---
+++ flushfinder-ui/src/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div>
+    <img src="./assets/block-m.png" width="50" height="50"></img>
+    <p>Bathroom</p>
+    </div>)
+};
--- flushfinder-ui/src/main.jsx
+++ flushfinder-ui/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import MyMap from './map.jsx'
+import Header from './header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header />
+    <MyMap />
     <App />
   </StrictMode>,
 )
