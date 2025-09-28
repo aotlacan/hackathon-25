0a6551fe7682e0b2ba4cb743eba46ce22b0b8288
root <dyang16688@gmail.com>
2025-09-27 20:42:15 -0400

    Header fix (CHECKPOINT)

--- flushfinder-ui/src/header.jsx
+++ flushfinder-ui/src/header.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Header() {
     return (
-        <div>
-    <img src="./assets/block-m.png" width="50" height="50"></img>
-    <p>Bathroom</p>
-    </div>)
+        <div id="MBathroomHeader">
+            <img src="./assets/block-m.png" width="50" height="50"></img>
+            <span>Bathroom</span>
+        </div>
+    )
 };
