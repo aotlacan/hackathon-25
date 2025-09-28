ce6a300426fdd4bb79a1615f323b91288329fe44
Alex Otlacan <aotlacan@gmail.com>
2025-09-28 11:10:35 -0400

    Added and changed rest of files

--- flushfinder-ui/src/header.jsx
+++ flushfinder-ui/src/header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import blockM from "./assets/block-m.png";
 
 export default function Header() {
     return (
         <div id="MBathroomHeader">
-            <img src="./assets/block-m.png" width="50" height="50"></img>
+            <img src={blockM} width="50" height="50"></img>
             <h1>Bathroom</h1>
         </div>
     )
