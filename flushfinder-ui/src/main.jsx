4c4c7ba19952ca0b668c3f19137eb90cf7a07444
Alex Otlacan <aotlacan@gmail.com>
2025-09-28 09:24:20 -0400

    checkpoint with webpage looking pretty good

--- flushfinder-ui/src/main.jsx
+++ flushfinder-ui/src/main.jsx
@@ -1,13 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import MyMap from './map.jsx'
-import Header from './header.jsx'
+// src/main.jsx
+import React, { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Header />
-    <App />
-  </StrictMode>,
-)
+import Header from "./header.jsx";
+import MyMap from "./map.jsx";
+import Rooms from "./Rooms.jsx";
+
+function Root() {
+  const [selectedBrn, setSelectedBrn] = useState("1005092");
+
+  return (
+    <StrictMode>
+      <div id="layout">
+        {/* Top header */}
+        <header id="MBathroomHeader">
+          <Header />
+        </header>
+
+        {/* Two-column content: map left, sidebar right */}
+        <div id="content">
+          <div id="map-section">
+            <MyMap onSelectBuilding={(brn) => setSelectedBrn(brn)} />
+          </div>
+
+          <aside id="sidebar">
+            <h2 style={{ marginTop: 0 }}>Rooms</h2>
+            <Rooms brn={selectedBrn} />
+          </aside>
+        </div>
+      </div>
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<Root />);
